c1f966fb2198ee4678e95fc914474e2bbb4a213d
Tristan Bouillot <tristanblt@gmail.com>
2020-03-09 22:15:25 +0100

    add fullscrenn mode

--- core/src/ArcadeCore/Menu.cpp
+++ core/src/ArcadeCore/Menu.cpp
@@ -20,4 +20,8 @@ void Menu::update(IBuilder *b)
     b->rectDraw({0, 0, 1600, 900}, b->hexToColor(0x212121FF));
     b->textDraw({"Unify", {50, 40}, 75, 2}, b->hexToColor(0xFFFFFFFF));
     b->rectDraw({0, 180, 1600, 600}, b->hexToColor(0x1A1A1AFF));
+    for (int i = 0; i < 5; i++) {
+        b->radiusRectDraw({0, 0, 50, 50}, 20, b->hexToColor(0x212100FF));
+    }
+    
 }
