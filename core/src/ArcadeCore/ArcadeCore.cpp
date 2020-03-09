e2b760c349824ecef92d76af2d1e1fbb073beef8
kenan <kenan.b4rb0t@gmail.com>
2020-03-10 00:17:11 +0100

    layout in progress

--- core/src/ArcadeCore/ArcadeCore.cpp
+++ core/src/ArcadeCore/ArcadeCore.cpp
@@ -23,7 +23,7 @@ void ArcadeCore::launchCore(DisplayLibrary *l)
     Layout layout;
     Builder b(l);
     Menu m;
-    bool isMenu = true;
+    bool isMenu = false;
 
     b.loadAsset("assets/fonts/Montserrat-Light.otf", AssetType::FONT);
     b.loadAsset("assets/fonts/Montserrat-Regular.otf", AssetType::FONT);
--- core/src/ArcadeCore/Layout.cpp
+++ core/src/ArcadeCore/Layout.cpp
@@ -17,5 +17,6 @@ Layout::~Layout()
 
 void Layout::update(IBuilder *b)
 {
-    b->rectDraw({0, ((float)(b->windowHeight() - b->toUnit(70))), (float)(b->windowWidth()), (float)(b->toUnit(70))}, b->hexToColor(0x555555FF));
+    b->rectDraw({0, ((b->windowHeight() - b->toUnit(b->windowHeight() / 12.8f))), b->windowWidth(), b->toUnit(b->windowHeight() / 12.8f)}, b->hexToColor(0x373737FF));
+    b->textDraw({"Unify", {b->windowWidth()/60, ((b->windowHeight() - b->toUnit(b->windowHeight() / 12.8f)))}, static_cast<int>(b->windowHeight() / 18.0f), 2}, b->hexToColor(0xFFFFFFFF));
 }
