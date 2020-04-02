c148913934be7adf46149f8566b08cd13b55d164
Tristan Bouillot <tristanblt@gmail.com>
2020-04-02 19:06:38 +0200

    opacuty in ncurses
    Former-commit-id: b5a4f959c1a52ac3fd0498958a89ef7233eed37c

--- lib/ncurses/src/Graphical/Window.cpp
+++ lib/ncurses/src/Graphical/Window.cpp
@@ -113,7 +113,7 @@ int Window::getColorPair(Color fg, Color bg)
 
 void Window::drawBufferPixel(int x, int y, Color color)
 {
-    if (y >= _height || x >= _width || x < 0 || y < 0)
+    if (y >= _height || x >= _width || x < 0 || y < 0 || color.a == 0)
         return;
     _colorBuffer[y][x] = color;
 }
